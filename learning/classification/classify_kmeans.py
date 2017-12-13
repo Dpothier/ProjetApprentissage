bba816efb7717d6392dc98c29dcf4958c9735bc4
Charline Lechailler <charlinelechailler@MacBook-Pro-de-Charline.local>
2017-12-13 11:21:14 -0500

    redaction de la classe classify_bernoulli_EM

--- learning/classification/classify_kmeans.py
+++ learning/classification/classify_kmeans.py
@@ -2,15 +2,6 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-# def test_for_n_clusters(n, data_vector, target):
-#     algo = KMeans(n_clusters=n, random_state=0)
-#     algo.fit(data_vector)
-#     predictions = algo.predict(data_vector)
-    
-#     silhouette_score = silhouette_score(data_vector, predictions)
-
-#     return n, silhouette_score
-
 
 class Clustering_kmeans:
 
