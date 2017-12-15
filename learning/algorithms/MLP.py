82bfd01a7b510edb165ce76e1a5f619d0f19fc33
Dominique Pothier <dpothier@live.ca>
2017-12-15 07:56:12 -0500

    Fin séparation hyperparamètres
    Fin de la séparation des hyperparamètres avec MLP et SVM

--- learning/algorithms/MLP.py
+++ learning/algorithms/MLP.py
@@ -6,45 +6,35 @@ from sklearn.model_selection import train_test_split
 
 
 class MLP:
-    def __call__(self, data, target):
-        data_experiment, data_validation, target_experiment, target_validation = train_test_split(data, target, test_size=0.2)
+    def __init__(self, sets_constructor):
+        self.sets_constructor = sets_constructor
+        self.current_layer = 0
+        self.current_neuron = 0
+
+
+    def produce_results(self, train_data, test_data, train_target, test_target):
+        return run_classifier(MLPClassifier(hidden_layer_sizes=(self.current_neuron,) * self.current_layer),
+                              train_data, test_data, train_target, test_target)
+
+    def optimize_hyperparameters(self, data, targets):
         layers = [1, 2, 3, 4, 5, 6, 7, 8]
         neurons_per_layer = [50, 75, 100, 125, 150, 175, 200]
 
-        results = np.zeros((len(layers), len(neurons_per_layer), 3))
-
+        best_result = (0,0,0)
+        best_result_hyperparameters = (0,0)
         for i in range(0, len(layers)):
             for j in range(0, len(neurons_per_layer)):
-                number_of_layers = layers[i]
-                number_of_neurons = neurons_per_layer[j]
-                kfold = KFold(n_splits=10)
-                training_accuracies = []
-                test_accuracies = []
-                execution_times = []
-                for train_indices, test_indices in kfold.split(data_experiment, target_experiment):
-                    result = run_classifier(MLPClassifier(hidden_layer_sizes=(number_of_neurons,) * number_of_layers),
-                                            data_experiment[train_indices],
-                                            data_experiment[test_indices],
-                                            target_experiment[train_indices],
-                                            target_experiment[test_indices])
+                self.current_layer = layers[i]
+                self.current_neuron = neurons_per_layer[j]
+                result = self.sets_constructor(data, targets, self.produce_results)
+                if result[0] > best_result[0]:
+                    best_result = result
+                    best_result_hyperparameters = (self.current_layer, self.current_neuron)
 
-                    training_accuracies.append(result[0])
-                    test_accuracies.append(result[1])
-                    execution_times.append(result[2])
+        #best_result = run_classifier(SVC(C=best_c, gamma=(1 / (2 * best_sigma ** 2))),
+        #                             data_experiment,
+        #                             data_validation,
+        #                             target_experiment,
+        #                             target_validation)
 
-                results[i, 0] = sum(training_accuracies) / len(training_accuracies)
-                results[i, 1] = sum(test_accuracies) / len(test_accuracies)
-                results[i, 2] = sum(execution_times) / len(execution_times)
-
-
-        best = np.unravel_index(np.argmax(results[:, :, 1]), results[:, :, 1].shape)
-        best_number_of_layer = layers[best[0]]
-        best_number_of_neurons = neurons_per_layer[best[1]]
-
-        best_result = run_classifier(MLPClassifier(hidden_layer_sizes=(best_number_of_neurons,) * best_number_of_layer),
-                                     data_experiment,
-                                     data_validation,
-                                     target_experiment,
-                                     target_validation)
-
-        return best_result + (best_number_of_layer, best_number_of_neurons)
+        return best_result + best_result_hyperparameters
--- learning/algorithms/SVM.py
+++ learning/algorithms/SVM.py
@@ -7,55 +7,40 @@ from sklearn.model_selection import train_test_split
 
 
 class SVM:
-    def __call__(self, data, target):
-        data_experiment, data_validation, target_experiment, target_validation = train_test_split(data, target,
-                                                                                                  test_size=0.2)
-        kfold = KFold(n_splits=10, shuffle=True)
+    def __init__(self, sets_constructor):
+        self.sets_constructor = sets_constructor
+        self.current_c = 0
+        self.current_sigma = 0
 
+
+    def produce_results(self, train_data, test_data, train_target, test_target):
+        return run_classifier(SVC(C=self.current_c, gamma=(1 / (2 * self.current_sigma ** 2))),
+                              train_data, test_data, train_target, test_target)
+
+    def optimize_hyperparameters(self, data, targets):
         cs = [1e-08, 1e-07, 1e-06, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
         min_sigma = self.get_min_sigma(data)
         sigmas = [min_sigma, 2 * min_sigma, 4 * min_sigma, 8 * min_sigma, 16 * min_sigma, 32 * min_sigma,
                   64 * min_sigma]
-        results = np.zeros((len(cs), len(sigmas), 3))
 
-        for train_index, test_index in kfold.split(data_experiment, target_experiment):
-            train_data = data[train_index]
-            train_target = target[train_index]
+        best_result = (0, 0, 0)
+        best_result_hyperparameters = (0,0)
+        for i in range(0, len(cs)):
+            for j in range(0, len(sigmas)):
+                self.current_c = cs[i]
+                self.current_sigma = sigmas[j]
+                result = self.sets_constructor(data, targets, self.produce_results)
+                if result[0] > best_result[0]:
+                    best_result = result
+                    best_result_hyperparameters = (self.current_c, self.current_sigma)
 
-            test_data = data[test_index]
-            test_target = target[test_index]
-            for i in range(0, len(cs)):
-                for j in range(0, len(sigmas)):
-                    c = cs[i]
-                    sigma = sigmas[j]
-                    training_accuracies = []
-                    test_accuracies = []
-                    execution_times = []
+        #best_result = run_classifier(SVC(C=best_c, gamma=(1 / (2 * best_sigma ** 2))),
+        #                             data_experiment,
+        #                             data_validation,
+        #                             target_experiment,
+        #                             target_validation)
 
-                    result = run_classifier(SVC(C=c, gamma=(1 / (2 * sigma ** 2))),
-                                            train_data,
-                                            test_data,
-                                            train_target,
-                                            test_target)
-
-                    training_accuracies.append(result[0])
-                    test_accuracies.append(result[1])
-                    execution_times.append(result[2])
-                    results[i, j, 0] = sum(training_accuracies) / len(training_accuracies)
-                    results[i, j, 1] = sum(test_accuracies) / len(test_accuracies)
-                    results[i, j, 2] = sum(execution_times) / len(execution_times)
-
-        best = np.unravel_index(np.argmax(results[:, :, 1]), results[:, :, 1].shape)
-        best_c = cs[best[0]]
-        best_sigma = sigmas[best[1]]
-
-        best_result = run_classifier(SVC(C=best_c, gamma=(1 / (2 * best_sigma ** 2))),
-                                     data_experiment,
-                                     data_validation,
-                                     target_experiment,
-                                     target_validation)
-
-        return best_result + (best_c, best_sigma)
+        return best_result + best_result_hyperparameters
 
     def get_min_sigma(self, data):
         distance_matrix = pairwise_distances(data, metric='cityblock')
