e0fa7936eda1d8d52ee1d28dccfdae72238cdf86
Dominique Pothier <dpothier@live.ca>
2017-12-15 14:54:52 -0500

    Corrections aux tests en généralisation

--- learning/generalisation_mlp_experiment.py
+++ learning/generalisation_mlp_experiment.py
@@ -10,27 +10,27 @@ data_test, targets_test = get_some_carcomplaints_data()
 
 dictionary = TerminologicalDictionary()
 
-setup.With_preselected_test_set(data_test, targets_test)\
+setup.With_kfold(10)\
     .use_MLP()\
-    .use_validation_set(0.2)\
+    .use_external_validation_set(data_test, targets_test)\
     .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
-    .output_to_file("../results/bdrv/mlp/count_postprocessing_single")\
+    .output_to_file("../results/generalisation/mlp/count_postprocessing_single")\
     .output_to_console()\
     .go(data, targets)
 
-setup.With_preselected_test_set(data_test, targets_test)\
+setup.With_kfold(10)\
     .use_MLP()\
-    .use_validation_set(0.2)\
+    .use_external_validation_set(data_test, targets_test)\
     .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
-    .output_to_file("../results/bdrv/mlp/count_postprocessing_stemming")\
+    .output_to_file("../results/generalisation/mlp/count_postprocessing_stemming")\
     .output_to_console()\
     .go(data, targets)
 
-setup.With_preselected_test_set(data_test, targets_test)\
+setup.With_kfold(10)\
     .use_MLP()\
-    .use_validation_set(0.2)\
+    .use_external_validation_set(data_test, targets_test)\
     .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
-    .output_to_file("../results/bdrv/mlp/count_postprocessing_lemma")\
+    .output_to_file("../results/generalisation/mlp/count_postprocessing_lemma")\
     .output_to_console()\
     .go(data, targets)
 
--- learning/generalisation_nb_experiment.py
+++ learning/generalisation_nb_experiment.py
@@ -14,7 +14,7 @@ setup.With_preselected_test_set(data_test, targets_test)\
     .use_nb()\
     .use_test_set_results()\
     .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
-    .output_to_file("../results/bdrv/nb/count_postprocessing_single")\
+    .output_to_file("../results/generalisation/nb/count_postprocessing_single")\
     .output_to_console()\
     .go(data, targets)
 
@@ -22,7 +22,7 @@ setup.With_preselected_test_set(data_test, targets_test)\
     .use_nb()\
     .use_test_set_results()\
     .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
-    .output_to_file("../results/bdrv/nb/count_postprocessing_stemming")\
+    .output_to_file("../results/generalisation/nb/count_postprocessing_stemming")\
     .output_to_console()\
     .go(data, targets)
 
@@ -30,7 +30,7 @@ setup.With_preselected_test_set(data_test, targets_test)\
     .use_nb()\
     .use_test_set_results()\
     .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
-    .output_to_file("../results/bdrv/nb/count_postprocessing_lemma")\
+    .output_to_file("../results/generalisation/nb/count_postprocessing_lemma")\
     .output_to_console()\
     .go(data, targets)
 
