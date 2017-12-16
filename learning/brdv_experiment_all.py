d385530aebd273fbbd71d89181106cea66f0edb3
Dominique Pothier <dpothier@live.ca>
2017-12-15 20:11:32 -0500

    Concaténer tous les tests pour bdrv

---
+++ learning/brdv_experiment_all.py
@@ -0,0 +1,130 @@
+from getData import get_bdrv_data
+import experiment.experiment_setup as setup
+import vectorization.VectorizerSet as sets
+from dictionary import TerminologicalDictionary
+
+
+data, targets = get_bdrv_data()
+
+dictionary = TerminologicalDictionary()
+
+setup.With_kfold(10)\
+    .use_ADABoost()\
+    .use_test_set_results()\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
+    .output_to_file("../results/bdrv/adaboost/count_postprocessing_single")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_ADABoost()\
+    .use_test_set_results()\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
+    .output_to_file("../results/bdrv/adaboost/count_postprocessing_stemming")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_ADABoost()\
+    .use_test_set_results()\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
+    .output_to_file("../results/bdrv/adaboost/count_postprocessing_lemma")\
+    .output_to_console()\
+    .go(data, targets)
+
+
+setup.With_kfold(10)\
+    .use_EM()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
+    .output_to_file("../results/bdrv/em/count_postprocessing_single")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_EM()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
+    .output_to_file("../results/bdrv/em/count_postprocessing_stemming")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_EM()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
+    .output_to_file("../results/bdrv/em/count_postprocessing_lemma")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_MLP()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
+    .output_to_file("../results/bdrv/mlp/count_postprocessing_single")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_MLP()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
+    .output_to_file("../results/bdrv/mlp/count_postprocessing_stemming")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_MLP()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
+    .output_to_file("../results/bdrv/mlp/count_postprocessing_lemma")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_nb()\
+    .use_test_set_results()\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
+    .output_to_file("../results/bdrv/nb/count_postprocessing_single")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_nb()\
+    .use_test_set_results()\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
+    .output_to_file("../results/bdrv/nb/count_postprocessing_stemming")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_nb()\
+    .use_test_set_results()\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
+    .output_to_file("../results/bdrv/nb/count_postprocessing_lemma")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_SVM()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_single(dictionary))\
+    .output_to_file("../results/bdrv/svm/count_postprocessing_single")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_SVM()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_stemming(dictionary))\
+    .output_to_file("../results/bdrv/svm/count_postprocessing_stemming")\
+    .output_to_console()\
+    .go(data, targets)
+
+setup.With_kfold(10)\
+    .use_SVM()\
+    .use_validation_set(0.2)\
+    .test_on_multiple_pretreatment(sets.count_postprocessing_lemma(dictionary))\
+    .output_to_file("../results/bdrv/svm/count_postprocessing_lemma")\
+    .output_to_console()\
+    .go(data, targets)
