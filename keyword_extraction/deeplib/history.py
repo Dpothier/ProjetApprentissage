09bc98ab1e28fff66bd00a7999e2f58481dbda1b
Dominique Pothier <dpothier@live.ca>
2018-05-10 16:56:51 -0400

    History skips the firsts epochs to produce a readable figure

--- keyword_extraction/deeplib/history.py
+++ keyword_extraction/deeplib/history.py
@@ -26,7 +26,7 @@ class History:
 
     def display(self):
         epoch = len(self.history['train_acc'])
-        epochs = [x for x in range(1, epoch + 1)]
+        epochs = [x for x in range(3, epoch + 1)]
 
         fig, axes = plt.subplots(2, 1)
         plt.tight_layout()
@@ -34,13 +34,13 @@ class History:
         axes[0].set_title('Train accuracy')
         axes[0].set_xlabel('Epochs')
         axes[0].set_ylabel('Accuracy')
-        axes[0].plot(epochs, self.history['train_acc'], label='Train')
-        axes[0].plot(epochs, self.history['val_acc'], label='Validation')
+        axes[0].plot(epochs, self.history['train_acc'][2:], label='Train')
+        axes[0].plot(epochs, self.history['val_acc'][2:], label='Validation')
         axes[0].legend()
 
         axes[1].set_title('Train loss')
         axes[1].set_xlabel('Epochs')
         axes[1].set_ylabel('Loss')
-        axes[1].plot(epochs, self.history['train_loss'], label='Train')
-        axes[1].plot(epochs, self.history['val_loss'], label='Validation')
+        axes[1].plot(epochs, self.history['train_loss'][2:], label='Train')
+        axes[1].plot(epochs, self.history['val_loss'][2:], label='Validation')
         plt.savefig(self.filename)
