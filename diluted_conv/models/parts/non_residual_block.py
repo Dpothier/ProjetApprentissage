7064c11b9f369661d62afb5d78df86ae11e97000
Dominique Pothier <dpothier@live.ca>
2019-04-26 15:52:40 -0400

    changements diluted conv

---
+++ diluted_conv/models/parts/non_residual_block.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from diluted_conv.models.parts.spatial_dropout import SpatialDropout
+from diluted_conv.models.parts.causal_conv import CausalConv1d
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_depth, out_depth, dilation=1, p=0.0):
+        super(ResidualBlock, self).__init__()
+        self.conv = nn.Conv1d(in_depth, in_depth, kernel_size=3, dilation=dilation, padding=dilation)
+        self.bn = nn.BatchNorm1d(in_depth)
+        self.dropout = SpatialDropout(p)
+
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+
+    def forward(self, x):
+        out = self.dropout(F.relu(self.bn(self.conv(x))))
+
+        return out
