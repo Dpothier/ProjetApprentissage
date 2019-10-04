c6173186791b65323a382411c8eec89674037469
Dominique Pothier <dpothier@live.ca>
2019-10-04 09:54:17 -0400

    Added tests for gpu support.

--- conditioned_layers/tests/PolicyMLP/MLP_Primitives_test.py
+++ conditioned_layers/tests/PolicyMLP/MLP_Primitives_test.py
@@ -135,6 +135,49 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(1.5, bias[1][2].item(), delta=0.1)
         self.assertAlmostEqual(1.7, bias[1][3].item(), delta=0.1)
 
+    # def test_gpu_deployment_set_weight(self):
+    #     self.MLP_Primitives_set_weight = self.MLP_Primitives_set_weight.gpu()
+    #     layer = self.MLP_Primitives_set_weight(self.attention_batched)
+    #
+    #
+    #     weights = layer.weight
+    #     bias = layer.bias
+    #
+    #     # First batch element is already tested in other case, so only test values of second element
+    #     self.assertAlmostEqual(2.3, weights[1][0][0].item(), delta=0.1)
+    #     self.assertAlmostEqual(5.3, weights[1][0][1].item(), delta=0.1)
+    #     self.assertAlmostEqual(8.3, weights[1][0][2].item(), delta=0.1)
+    #     self.assertAlmostEqual(11.3, weights[1][0][3].item(), delta=0.1)
+    #
+    #     self.assertAlmostEqual(1.9, weights[1][1][0].item(), delta=0.1)
+    #     self.assertAlmostEqual(4.9, weights[1][1][1].item(), delta=0.1)
+    #     self.assertAlmostEqual(7.9, weights[1][1][2].item(), delta=0.1)
+    #     self.assertAlmostEqual(10.9, weights[1][1][3].item(), delta=0.1)
+    #
+    #     self.assertAlmostEqual(1.5, weights[1][2][0].item(), delta=0.1)
+    #     self.assertAlmostEqual(4.5, weights[1][2][1].item(), delta=0.1)
+    #     self.assertAlmostEqual(7.5, weights[1][2][2].item(), delta=0.1)
+    #     self.assertAlmostEqual(10.5, weights[1][2][3].item(), delta=0.1)
+    #
+    #     self.assertAlmostEqual(1.7, weights[1][3][0].item(), delta=0.1)
+    #     self.assertAlmostEqual(4.7, weights[1][3][1].item(), delta=0.1)
+    #     self.assertAlmostEqual(7.7, weights[1][3][2].item(), delta=0.1)
+    #     self.assertAlmostEqual(10.7, weights[1][3][3].item(), delta=0.1)
+    #
+    #     self.assertAlmostEqual(2.3, bias[1][0].item(), delta=0.1)
+    #     self.assertAlmostEqual(1.9, bias[1][1].item(), delta=0.1)
+    #     self.assertAlmostEqual(1.5, bias[1][2].item(), delta=0.1)
+    #     self.assertAlmostEqual(1.7, bias[1][3].item(), delta=0.1)
+    #
+    # def test_gpu_deployment_random_weights(self):
+    #     self.MLP_primitives_random_weight = self.MLP_primitives_random_weight.gpu()
+    #     layer = self.MLP_primitives_random_weight(self.attention_full_layer)
+    #
+    #     self.assertEqual(1, layer.weight.size()[0])
+    #     self.assertEqual(self.state_size, layer.weight.size()[1])
+    #     self.assertEqual(self.state_size, layer.weight.size()[2])
+    #     self.assertEqual(layer.bias.size()[1], self.state_size)
+
 
 
 if __name__ == '__main__':
