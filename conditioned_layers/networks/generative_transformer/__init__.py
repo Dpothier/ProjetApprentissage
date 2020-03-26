93cfb34fd68f3f673716bf6e186bd39113c7be0f
Dominique <dpothier@live.ca>
2020-03-26 15:50:58 -0400

    Removed transformers

--- conditioned_layers/networks/generative_transformer/__init__.py
+++
@@ -1,9 +0,0 @@
-# import conditioned_layers.networks.transformer.Constants as Constants
-# import conditioned_layers.networks.transformer.Modules as Modules
-# import conditioned_layers.networks.transformer.Layers as Layers
-# import conditioned_layers.networks.transformer.SubLayers as SubLayers
-# import conditioned_layers.networks.transformer.Models as Models
-#
-# __all__ = [
-#     Constants, Modules, Layers,
-#     SubLayers, Models]
